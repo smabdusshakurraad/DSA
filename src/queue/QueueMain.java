af23265a2ca923c7236156f9bb82f704dcf63c36
Abdus Sakur <abdusshakurraad@gmail.com>
2023-05-02 01:57:56 +0600

    queue problem solving

---
+++ src/queue/QueueMain.java
@@ -0,0 +1,32 @@
+package queue;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
+public class QueueMain {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        var front = queue.remove();
+        System.out.println(front);
+        System.out.println(queue);
+        reverse(queue);
+    }
+
+    public static void reverse(Queue<Integer> queue){
+        Stack<Integer> stack = new Stack<>();
+
+        while (!queue.isEmpty()){
+            stack.push(queue.remove());
+        }
+
+        while (!stack.empty()){
+            queue.add(stack.pop());
+        }
+
+        System.out.println(queue);
+    }
+}
