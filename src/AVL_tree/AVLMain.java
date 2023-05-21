6fb0c4c720bb363e20c3f6a13a1ba7b0508b2fa4
Abdus Sakur <abdusshakurraad@gmail.com>
2023-05-22 02:41:06 +0600

    AVL tree recursive insertion

---
+++ src/AVL_tree/AVLMain.java
@@ -0,0 +1,12 @@
+package AVL_tree;
+
+public class AVLMain {
+    public static void main(String[] args) {
+        AVLTree avlTree = new AVLTree();
+        avlTree.insert(10);
+        avlTree.insert(20);
+        avlTree.insert(30);
+
+        System.out.println();
+    }
+}
