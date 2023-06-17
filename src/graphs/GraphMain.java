095fb9a5485c6e71e21689bc483b1bd1fa21fe53
Abdus Sakur <abdusshakurraad@gmail.com>
2023-06-18 01:38:18 +0600

    Graphs implementations

---
+++ src/graphs/GraphMain.java
@@ -0,0 +1,17 @@
+package graphs;
+
+public class GraphMain {
+    public static void main(String[] args) {
+        var graph = new Graph();
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+        graph.addEdge("A", "B");
+        graph.addEdge("A", "C");
+        graph.removeNode("B");
+        graph.print();
+
+
+
+    }
+}
